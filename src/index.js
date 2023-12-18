cf575f88e60b0691650598b2cfe567708b78d63b
Julian Lim Kah Chun <jullim@deloitte.com>
2023-12-18 17:27:42 +0800

    Removed unneeded files and created starting Header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Header from './Components/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Header />
 );
 
 // If you want to start measuring performance in your app, pass a function
